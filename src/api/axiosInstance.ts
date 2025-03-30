6aa8b4a8a0c449153c23174e8b1429146bbc11a5
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2025-03-30 13:32:24 +0530

    jenkinsfile add

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8090/api", // Replace with your base URL
+  baseURL: "http://65.0.87.234:8081/api", // Replace with your base URL
   withCredentials: true, // Include cookies in requests
   headers: {
     "Content-Type": "application/json", // Set the Content-Type
