3123abb5ab29dbb515e50e5b8501532d8e8ccf83
Ashfaq Riyaldeen <ashfaqriyaldeen@gmail.com>
2025-04-04 14:16:22 +0530

    normal trigger

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -88,7 +88,7 @@ const BlogHomePage: React.FC = () => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5 }}
             >
-              Share Your Love Story,  <br />
+              Share Your Story,  <br />
               <span className="text-primary">Inspire the World</span>
             </motion.h1>
             <motion.p 
